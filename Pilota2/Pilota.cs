80d7dc6dfa5da5fd65835465b7b3f939b90f6cd7
KissAniko <126045521+KissAniko@users.noreply.github.com>
2023-09-07 15:09:36 +0200

    Add project files.

---
+++ Pilota2/Pilota.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilota2
+{
+    internal class Pilota
+    {
+        string nev;
+        //DateTime szuldatum;
+        string szuldatum;
+        string nemzetiseg;
+        string rajtszam;
+
+        public Pilota(string nev, string szuldatum, string nemzetiseg, string rajtszam = "")
+        {
+           //  throw new InvalidOperationException("Nem sikerült kinyerni az évszámot az szuldatum-ból.");
+
+            this.nev = nev;
+            this.szuldatum = szuldatum;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+       
+        // Osztályokosítás:
+        public string Nev { get => nev; set => nev = value; }
+      
+        public string Nemzetiseg { get => nemzetiseg; set => nemzetiseg = value; }
+        public string Rajtszam { get => rajtszam; set => rajtszam = value; }
+
+       // public int SzulEv { get => Convert.ToInt32(szuldatum.Substring(4)); }  // Substring ---> leválasztás, vagy kivágás.
+       //                                                                     .. jelen esetben az első 4 karaktert választom le
+                                                                         //   ...vagyis az évszámot, amit meg számmá alakítok.
+
+        //1991.01.01
+        public int SzulEv { get => int.Parse(szuldatum.Substring(0, 4)); }   // ... vagy így is át lehet alakítani számmá, mindkét eset jó.
+                   
+            
+        public int SzulHonap { get => int.Parse(szuldatum.Substring(6, 2)); }  // Itt az 5. után lévők közül 2 karakter kell.
+        public int SzulNap { get => int.Parse(szuldatum.Substring(9, 2)); }    // Itt a 8. uáni karaktertől számítva szintén 2 karakter kell.
+
+                                                                             // Tehát azt írjuk elsőször, hogy melyik karakter utántól számolja,
+                                                                             //...  utána pedig  azt, amennyire szükségünk van. 
+
+       
+    }
+}
---
+++ Pilota2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilota2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pilota> pilotak = new List<Pilota>();
+             File.ReadAllLines("pilotak.csv").Skip(1);
+
+            string[] sorok = File.ReadAllLines("pilotak.csv");
+            foreach (var sor in sorok) {
+
+              //  Console.WriteLine( sor);  
+            }
+           
+            Console.WriteLine($"3.feladat: {sorok.Length-1}");
+            Console.WriteLine($"3.feladat: {sorok.Count()-1}");
+            Console.WriteLine($"4.feladat: {sorok.Last().Split(';')[0]}");
+      
+
+            string[] adatok = File.ReadAllLines("pilotak.csv");
+
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string[] adatsorok = adatok[i].Split(';');
+               //  Pilota tagok = new Pilota(adatsorok[0], Convert.ToDateTime(adatsorok[1]), adatsorok[2], adatsorok[3]);
+                Pilota tagok = new Pilota(adatsorok[0], (adatsorok[1]), adatsorok[2], adatsorok[3]);
+                pilotak.Add(tagok);
+              
+            }
+            Console.WriteLine($"4.feladat: {pilotak[pilotak.Count - 1].Nev}");
+
+
+            // 5. feladatban leválogatás és soronkénti tesztelés
+            foreach(var pilota in pilotak) {
+                if (pilota.SzulEv < 1901)
+                {
+                    Console.WriteLine($"5.feladat:\t{pilota.Nev} ({pilota.SzulEv}. {pilota.SzulHonap}. {pilota.SzulNap}.)");
+
+                }
+            }  
+
+            
+        }
+    }
+}
